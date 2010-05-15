0fc407d8faf8305c13a239c5b6e45c63c329465b
John SJ Anderson <genehack@genehack.org>
2010-05-15 11:10:33 -0400

    master config file

---
+++ init.el
@@ -0,0 +1,67 @@
+;; init.el -- master configuration file
+
+;; VARIABLES
+;;; if you're not me, you probably want to change this to point to
+;;; where you put your checkout of the github repo:
+(defvar genehack/emacs-dir (expand-file-name "~/proj/emacs/")
+  "directory containing working copy of 'emacs' repo")
+
+(defvar genehack/emacs-config-dir (concat genehack/emacs-dir "etc/")
+  "sub-directory containing config files")
+(add-to-list 'load-path genehack/emacs-config-dir)
+
+(defvar genehack/emacs-libs-dir (concat genehack/emacs-dir "lib/")
+  "sub-directory containing third-party emacs libs")
+(add-to-list 'load-path genehack/emacs-libs-dir)
+
+;;; I generally don't use the Customize interface, but sometimes
+;;; things get added there. Setting this means the file is under
+;;; revision control, so if something touches it, I'll notice.
+(setq custom-file (concat genehack/emacs-config-dir "custom.el"))
+
+;; DEFAULT FACE
+;;; If you don't set this early on, sometimes things get wonky.
+(set-face-attribute 'default t
+                    :background "#000000"
+                    :foreground "#ffffff"
+                    :family "Consolas"
+                    :height 161)
+
+;; ELPA
+(require 'package)
+(setq package-user-dir (concat genehack/emacs-dir "elpa"))
+;;; initialize ELPA, creating package directory if necessary
+;;; (and complaining if we're blocked by a file...)
+(if (file-exists-p package-user-dir)
+    (if (file-directory-p package-user-dir)
+        (package-initialize)
+      (error "ELPA package dir creation blocked by file"))
+  (make-directory package-user-dir))
+
+;; PRIVATE CONFIG
+;;; Again, you might want to change this path to a private file of
+;;; your own. This is a place to put variables that set passwords,
+;;; email addresses, and other stuff that you don't want in a public
+;;; github repo...
+(defvar genehack/private-config-file "~/private/emacs-private.el"
+  "file with configuration info that can't be in public repository.")
+(if (file-readable-p genehack/private-config-file)
+    (load-library genehack/private-config-file))
+
+;; MODULES
+;;; All the rest of the config is split out into individual files, for
+;;; ease of use.
+(defvar genehack/module-list
+  '(
+    "builtins"
+    ;; "erc"
+    ;; "gnus"
+    "keys"
+    "misc"
+    "perl"
+    )
+  "list of modules to load on startup.")
+
+(dolist (pkg genehack/module-list)
+  (if (file-readable-p (concat genehack/emacs-config-dir pkg ".el"))
+      (load-library pkg)))
