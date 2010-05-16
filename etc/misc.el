44ad31a6f8cab49889035bd90163578d37b49cfc
John SJ Anderson <genehack@genehack.org>
2010-05-15 23:00:13 -0400

    ack lib

--- etc/misc.el
+++ etc/misc.el
@@ -1,6 +1,9 @@
 ;; misc.el -- various customizations and additions
 ;;; This is for stuff that _isn't_ built-in to Emacs
 
+;;; ACK
+(require 'ack)
+
 ;;; AUTO COMPLETE
 (add-to-list 'load-path (concat genehack/emacs-libs-dir "auto-complete"))
 (require 'auto-complete-config)
