49027a48a285277d5a63247ea526a8515ab0b0f6
John SJ Anderson <genehack@genehack.org>
2010-07-16 13:53:52 -0400

    install tangotango

--- etc/misc.el
+++ etc/misc.el
@@ -29,10 +29,46 @@
 ;;; COLOR-THEME
 (add-to-list 'load-path (concat genehack/emacs-libs-dir "color-theme"))
 (require 'color-theme)
+(setq color-theme-load-all-themes nil)
 (color-theme-initialize)
+(require 'color-theme)
 (require 'color-theme-twilight)
 (require 'color-theme-hober2)
-(color-theme-hober2)
+(require 'color-theme-tangotango)
+
+;; select theme - first list element is for windowing system, second is for console/terminal
+;; Source : http://www.emacswiki.org/emacs/ColorTheme#toc9
+(setq color-theme-choices
+      '(color-theme-tangotango color-theme-tangotango))
+
+;; default-start
+(funcall (lambda (cols)
+           (let ((color-theme-is-global nil))
+             (eval
+              (append '(if (window-system))
+                      (mapcar (lambda (x) (cons x nil))
+                              cols)))))
+         color-theme-choices)
+
+;; test for each additional frame or console
+(require 'cl)
+(fset 'test-win-sys
+      (funcall (lambda (cols)
+ (lexical-let ((cols cols))
+ (lambda (frame)
+   (let ((color-theme-is-global nil))
+     ;; must be current for local ctheme
+     (select-frame frame)
+     ;; test winsystem
+     (eval
+      (append '(if (window-system frame))
+              (mapcar (lambda (x) (cons x nil))
+                      cols)))))))
+               color-theme-choices ))
+;; hook on after-make-frame-functions
+(add-hook 'after-make-frame-functions 'test-win-sys)
+
+(color-theme-tangotango)
 
 ;;; CSS-HEXCOLOR
 (require 'css-hexcolor)
