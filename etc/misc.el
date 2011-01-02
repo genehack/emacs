2a4c546c4cbca49a52f55ebb64f422a3f804476d
John SJ Anderson <genehack@genehack.org>
2011-01-01 22:45:09 -0500

    fix js2-mode invocation

--- etc/misc.el
+++ etc/misc.el
@@ -126,7 +126,7 @@
 (autoload 'tidy-build-menu         "tidy" "Install an options menu for HTML Tidy." t)
 
 ;;; JS2
-(autoload 'js2-mode "js2" nil t)
+(autoload 'js2-mode "js2-mode" nil t)
 (add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
 (setq-default js2-basic-offset 2)
 
