14c7a1fab75ef65dafabe6b4dddb9eca5c3155a2
John SJ Anderson <genehack@genehack.org>
2011-01-09 18:50:07 -0500

    utility script

---
+++ bin/wget-anything.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+cd ~/proj/emacs/lib
+wget -nd -np -N \
+http://www.emacswiki.org/emacs/download/anything.el \
+http://www.emacswiki.org/emacs/download/anything-config.el \
+http://www.emacswiki.org/emacs/download/anything-match-plugin.el
+# http://www.emacswiki.org/emacs/download/anything-extension.el \
+# http://www.emacswiki.org/emacs/download/descbinds-anything.el \
+# http://www.emacswiki.org/emacs/download/anything-complete.el \
+# http://www.emacswiki.org/emacs/download/anything-etags.el \
+# http://www.emacswiki.org/emacs/download/anything-ipa.el \
+# http://www.emacswiki.org/emacs/download/anything-yaetags.el \
+# http://www.emacswiki.org/emacs/download/anything-gtags.el \
+# http://www.emacswiki.org/emacs/download/anything-dabbrev-expand.el \
+# http://www.emacswiki.org/emacs/download/anything-traverse.el \
+# http://www.emacswiki.org/emacs/download/anything-mercurial.el \
+# http://www.emacswiki.org/emacs/download/anything-delicious.el \
+# http://www.emacswiki.org/emacs/download/AnythingConfigThierryVolpiatto.el \
+# http://www.emacswiki.org/emacs/download/download/anything-traverse.el
