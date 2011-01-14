3d7e85875704cc2fdbc1a4e660a284c7ab083099
John SJ Anderson <genehack@genehack.org>
2011-01-14 10:21:09 -0500

    frob various org-mode options

--- etc/org-conf.el
+++ etc/org-conf.el
@@ -4,12 +4,23 @@
 
 (add-to-list 'auto-mode-alist '("\\.org\\'" . org-mode))
 
-(setq org-log-done t
-      org-startup-indented 1
-      org-agenda-files '("~/Dropbox/org/")
+(setq org-agenda-files '("~/Dropbox/org/")
+      org-agenda-skip-deadline-if-done t
+      org-agenda-skip-scheduled-if-done t
+      org-agenda-start-on-weekday 0
+      org-blank-before-new-entry
+      '((heading . t) (plain-list-item . nil))
       org-directory (expand-file-name "~/Dropbox/org/")
       org-default-notes-file (concat org-directory "/capture.org")
+      org-log-done t
+      org-mobile-inbox-for-pull "~/Dropbox/org/capture.org"
+      org-mobile-directory "~/Dropbox/MobileOrg"
       org-refile-targets '((org-agenda-files :level . 1))
+      org-refile-use-outline-path 'file
+      org-return-follows-link t
+      org-startup-folded nil
+      org-startup-indented 1
+      org-M-RET-may-split-line '((headline nil)(item nil)(default t))
       )
 
 
