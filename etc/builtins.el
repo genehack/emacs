a467b9923ef7eab0aae117113d94fca9ac3bbd02
John SJ Anderson <genehack@genehack.org>
2011-01-16 22:43:21 -0500

    set up uniquify

--- etc/builtins.el
+++ etc/builtins.el
@@ -270,7 +270,9 @@ This is a buffer-local variable.")
 (setq delete-by-moving-to-trash t)
 
 ;;; UNIQUIFY
-(setq uniquify-buffer-name-style 'forward
+(require 'uniquify)
+(setq uniquify-buffer-name-style 'reverse
+      uniquify-separator "/"
       uniquify-after-kill-buffer-p t
       uniquify-ignore-buffers-re "^\\*")
 
