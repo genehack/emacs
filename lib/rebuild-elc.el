d04b9f23378d3fc5037413058c7aab5c285a45c1
John SJ Anderson <john.jacobsanderson@nih.gov>
2011-03-07 09:40:45 -0500

    update to rebuild anything

--- lib/rebuild-elc.el
+++ lib/rebuild-elc.el
@@ -1,5 +1,17 @@
 ;; run with emacs -batch rebuild-elc.el  -kill
 
-(cd "~/proj/emacs")
-(byte-compile-file "lib/smart-tab/smart-tab.el")
+(defvar genehack/rebuild-elc-el-files-list
+  '(
+    "lib/smart-tab/smart-tab.el"
+    "lib/anything.el"
+    "lib/anything-config.el"
+    "lib/anything-match-plugin.el"
+    )
+  "list of files to rebuild." )
 
+(cd "~/proj/emacs")
+
+(add-to-list 'load-path (expand-file-name "./lib"))
+
+(dolist (file genehack/rebuild-elc-el-files-list)
+  (byte-compile-file file))
