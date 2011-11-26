e2d7ded8efbba2b28b6fa79d2e6e10113ae3452b
John SJ Anderson <johna@farematrix.com>
2011-11-26 11:14:31 -0500

    turn on autopair in cperl

--- etc/perl.el
+++ etc/perl.el
@@ -20,6 +20,7 @@
 ;;; HOOK MODS
 (add-hook 'cperl-mode-hook 'genehack/cperl-mode-setup)
 (add-hook 'cperl-mode-hook 'genehack/perl-auto-complete-setup)
+(add-hook 'cperl-mode-hook 'autopair-mode)
 (add-hook 'cperl-mode-hook 'flyspell-prog-mode)
 (add-hook 'cperl-mode-hook 'flymake-mode)
 (add-hook 'cperl-mode-hook 'linum-mode)
