c5600c9d01db5fcb16fc4569d7cfce52ed2928ca
John SJ Anderson <genehack@genehack.org>
2012-03-17 11:07:53 -0400

    Remove lib/bbdb and other gnus related stuff

--- lib/bbdb/lisp/bbdb-autoloads.el
+++
@@ -1,15 +0,0 @@
-(provide 'bbdb-autoloads)
-(eval-when-compile
-  (condition-case ()
-      (require 'custom)
-    (error nil))
-  (if (and (featurep 'custom) (fboundp 'custom-declare-variable))()
-    (defmacro defgroup (&rest args)
-      nil)
-    (defmacro defcustom (var value doc &rest args)
-      `(defvar ,var ,value ,doc))
-    (defmacro defface (var value doc &rest args)
-      `(make-face ,var))
-    (defmacro define-widget (&rest args)
-      nil)))
-
