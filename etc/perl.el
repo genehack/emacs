bd3cd2f17eb2febba39e5512225be75ae16d7dc0
John SJ Anderson <genehack@genehack.org>
2014-08-27 14:15:47 -0700

    Add .psgi to perl-mode extensions

--- etc/perl.el
+++ etc/perl.el
@@ -15,6 +15,7 @@
 (defalias 'perl-mode 'cperl-mode)
 
 (add-to-list 'auto-mode-alist '("\\.cgi$" . cperl-mode))
+(add-to-list 'auto-mode-alist '("\\.psgi$" . cperl-mode))
 (add-to-list 'auto-mode-alist '("\\.t$" . cperl-mode))
 (add-to-list 'auto-mode-alist '("\\.pod$" . pod-mode))
 
