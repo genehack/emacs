e47e39e13cb443c16a2bc5d1eae38f762c57d948
John SJ Anderson <genehack@genehack.org>
2014-09-16 18:21:38 -0700

    some helm keybindings to make it less annoying

--- etc/misc.el
+++ etc/misc.el
@@ -188,6 +188,10 @@ RequireFilenameMatchPackage policy works properly.
 (require 'helm-grep)
 (require 'helm-projectile)
 
+(define-key helm-map (kbd "<tab>") 'helm-execute-persistent-action) ; rebihnd tab to do persistent action
+(define-key helm-map (kbd "C-i") 'helm-execute-persistent-action) ; make TAB works in terminal
+(define-key helm-map (kbd "C-j")  'helm-select-action) ; list actions using C-j
+
 (setq
  helm-google-suggest-use-curl-p t
  helm-scroll-amount 4
