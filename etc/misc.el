0b21c55601b909bf6da84dc5c4772b4a2986c08c
John SJ Anderson <genehack@genehack.org>
2017-01-02 11:32:13 -0800

    Add web-beautify config
    From https://www.reddit.com/r/emacs/comments/4xiaym/packages_for_javascript/

--- etc/misc.el
+++ etc/misc.el
@@ -654,6 +654,21 @@ given a prefix arg ARG, unconditionally use `ido-find-file`."
    "perl -MURI::Escape -e 'print URI::Escape::uri_unescape(do { local $/; <STDIN> })'"
    'current-buffer t))
 
+;;; WEB-BEAUTIFY
+(use-package web-beautify
+  :ensure web-beautify
+  :config (progn
+            (eval-after-load 'js2-mode
+              '(define-key js2-mode-map (kbd "C-c b") 'web-beautify-js))
+            (eval-after-load 'json-mode
+              '(define-key json-mode-map (kbd "C-c b") 'web-beautify-js))
+            (eval-after-load 'sgml-mode
+              '(define-key html-mode-map (kbd "C-c b") 'web-beautify-html))
+            (eval-after-load 'web-mode
+              '(define-key web-mode-map (kbd "C-c b") 'web-beautify-html))
+            (eval-after-load 'css-mode
+              '(define-key css-mode-map (kbd "C-c b") 'web-beautify-css))))
+
 ;;; WEB-MODE
 (use-package web-mode
   :config (progn
