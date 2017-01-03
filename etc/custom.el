128d2fcaa187f1c91b7cb58e4843a9be2e6ffcdc
John SJ Anderson <john@genehack.org>
2017-01-02 16:27:05 -0800

    local vars

--- etc/custom.el
+++ etc/custom.el
@@ -5,7 +5,11 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(safe-local-variable-values
+   (quote
+    ((js2-basic-offset . 4)
+     (eval progn
+           (genehack/nvm "4.7"))))))
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
