3f4b4e5970198b837623102d78a7c07886d418ee
John SJ Anderson <john@genehack.org>
2017-02-18 17:21:27 -0500

    custom crap

--- etc/custom.el
+++ etc/custom.el
@@ -5,11 +5,16 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (yaml-mode web-mode magit git-commit use-package web-beautify nvm js2-refactor company-tern twilight-theme tidy smex smart-tab perlcritic names multi-term kolon-mode js2-mode github-browse-file genehack-perl-elisp genehack-misc-elisp flycheck-color-mode-line flx-ido expand-region exec-path-from-shell disk dired-details+ company-go browse-kill-ring ag)))
  '(safe-local-variable-values
    (quote
     ((js2-basic-offset . 4)
      (eval progn
-           (genehack/nvm "4.7"))))))
+           (genehack/nvm "4.7")))))
+ '(term-default-bg-color "#000000")
+ '(term-default-fg-color "#cccccc"))
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
