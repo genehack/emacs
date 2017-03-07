4990246651096c613b391954a3b4e636985c4174
John SJ Anderson <john@genehack.org>
2017-03-07 11:49:01 -0800

    fix custom fuckery

--- etc/custom.el
+++ etc/custom.el
@@ -10,7 +10,7 @@
     (aggressive-indent yaml-mode web-mode web-beautify use-package twilight-theme tidy swift-mode solarized-theme smex smartparens smart-tab projectile php-mode perlcritic paredit nvm multi-term move-text markdown-mode magit less-css-mode kolon-mode json-mode js2-refactor github-browse-file genehack-perl-elisp genehack-misc-elisp flymake-php flycheck-color-mode-line flx-ido expand-region exec-path-from-shell disk dired-details+ dash-at-point company-tern company-go browse-kill-ring ag)))
  '(safe-local-variable-values
    (quote
-    (yaml-indent-offset . 4)
+    ((yaml-indent-offset . 4)
     (js2-basic-offset . 4)
     (eval progn
           (genehack/nvm "4.7")))))
