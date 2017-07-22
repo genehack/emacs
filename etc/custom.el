30338fad61d9c8295b6efac23467fd6501c3166a
John SJ Anderson <john@genehack.org>
2017-07-22 11:47:45 -0700

    add graphviz-dot-mode

--- etc/custom.el
+++ etc/custom.el
@@ -7,7 +7,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (markdown-mode csharp-mode omnisharp cobra-mode flymake-haml haml-mode flycheck-swift flycheck-swift3 aggressive-indent yaml-mode web-mode web-beautify use-package twilight-theme tidy swift-mode solarized-theme smex smartparens smart-tab projectile php-mode perlcritic paredit nvm multi-term move-text magit less-css-mode kolon-mode json-mode js2-refactor github-browse-file genehack-perl-elisp genehack-misc-elisp flymake-php flycheck-color-mode-line flx-ido expand-region exec-path-from-shell disk dired-details+ dash-at-point company-tern company-go browse-kill-ring ag)))
+    (graphviz-dot-mode markdown-mode csharp-mode omnisharp cobra-mode flymake-haml haml-mode flycheck-swift flycheck-swift3 aggressive-indent yaml-mode web-mode web-beautify use-package twilight-theme tidy swift-mode solarized-theme smex smartparens smart-tab projectile php-mode perlcritic paredit nvm multi-term move-text magit less-css-mode kolon-mode json-mode js2-refactor github-browse-file genehack-perl-elisp genehack-misc-elisp flymake-php flycheck-color-mode-line flx-ido expand-region exec-path-from-shell disk dired-details+ dash-at-point company-tern company-go browse-kill-ring ag)))
  '(safe-local-variable-values
    (quote
     ((yaml-indent-offset . 4)
