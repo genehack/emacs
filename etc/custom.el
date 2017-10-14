84efedf749e5ccc32e1f2d373b83feea3bd91ee7
John SJ Anderson <john@genehack.org>
2017-10-14 14:35:26 -0700

    Always use color graphs in magit logs

--- etc/custom.el
+++ etc/custom.el
@@ -5,6 +5,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(magit-log-arguments (quote ("--graph" "--color" "--decorate" "-n256")))
  '(package-selected-packages
    (quote
     (helpful all-the-icons all-the-icons-dired all-the-icons-ivy counsel-dash counsel-projectile counsel swiper ivy apib-mode nodejs-repl dumb-jump graphviz-dot-mode markdown-mode csharp-mode omnisharp cobra-mode flymake-haml haml-mode flycheck-swift flycheck-swift3 aggressive-indent yaml-mode web-mode web-beautify use-package twilight-theme tidy swift-mode solarized-theme smex smartparens smart-tab projectile php-mode perlcritic paredit nvm multi-term move-text magit less-css-mode kolon-mode json-mode js2-refactor github-browse-file genehack-perl-elisp genehack-misc-elisp flymake-php flycheck-color-mode-line flx-ido expand-region exec-path-from-shell disk dired-details+ dash-at-point company-tern company-go browse-kill-ring ag)))
